aa0fde3919e665c16c9e0d4ec4d557cb9e123d56
Arnav Mehta <arnavdatt@gmail.com>
2024-09-10 18:26:20 +0100

    Added enum class bitwise operator enabling macro

--- Ryu/Core/ObjectMacros.h
+++ Ryu/Core/ObjectMacros.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <type_traits>
 
 #define RYU_DISABLE_COPY(T)      \
 	explicit T(const T&) = delete; \
@@ -22,3 +23,78 @@
 #define UNLIKELY [[unlikely]]
 #define MAYBE_UNUSED [[maybe_unused]]
 #define NO_RETURN [[noreturn]]
+
+// Macro to define bitwise operators for enum classes
+#define RYU_ENABLE_ENUM_OPERATORS(T)                                                                     \
+	constexpr T operator|(T lhs, T rhs)                                                                  \
+	{                                                                                                    \
+		return static_cast<T>(                                                                           \
+			static_cast<std::underlying_type_t<T>>(lhs) | static_cast<std::underlying_type_t<T>>(rhs));  \
+	}                                                                                                    \
+	constexpr T operator&(T lhs, T rhs)                                                                  \
+	{                                                                                                    \
+		return static_cast<T>(                                                                           \
+			static_cast<std::underlying_type_t<T>>(lhs) & static_cast<std::underlying_type_t<T>>(rhs));  \
+	}                                                                                                    \
+	constexpr T operator^(T lhs, T rhs)                                                                  \
+	{                                                                                                    \
+		return static_cast<T>(                                                                           \
+			static_cast<std::underlying_type_t<T>>(lhs) ^ static_cast<std::underlying_type_t<T>>(rhs));  \
+	}                                                                                                    \
+	constexpr T operator~(T lhs)                                                                         \
+	{                                                                                                    \
+		return static_cast<T>(~static_cast<std::underlying_type_t<T>>(lhs));                             \
+	}                                                                                                    \
+	inline T& operator|=(T& lhs, T rhs)                                                                  \
+	{                                                                                                    \
+		lhs = lhs | rhs;                                                                                 \
+		return lhs;                                                                                      \
+	}                                                                                                    \
+	inline T& operator&=(T& lhs, T rhs)                                                                  \
+	{                                                                                                    \
+		lhs = lhs & rhs;                                                                                 \
+		return lhs;                                                                                      \
+	}                                                                                                    \
+	inline T& operator^=(T& lhs, T rhs)                                                                  \
+	{                                                                                                    \
+		lhs = lhs ^ rhs;                                                                                 \
+		return lhs;                                                                                      \
+	}
+
+
+// Macro to define bitwise operators for enum classes, returning the underlying type
+#define RYU_ENABLE_ENUM_OPERATORS_UNDERLYING(T)                                                           \
+	constexpr std::underlying_type_t<T> operator|(T lhs, T rhs)                                           \
+	{                                                                                                     \
+		return static_cast<std::underlying_type_t<T>>(lhs) | static_cast<std::underlying_type_t<T>>(rhs); \
+	}                                                                                                     \
+	constexpr std::underlying_type_t<T> operator&(T lhs, T rhs)                                           \
+	{                                                                                                     \
+		return static_cast<std::underlying_type_t<T>>(lhs) & static_cast<std::underlying_type_t<T>>(rhs); \
+	}                                                                                                     \
+	constexpr std::underlying_type_t<T> operator^(T lhs, T rhs)                                           \
+	{                                                                                                     \
+		return static_cast<std::underlying_type_t<T>>(lhs) ^ static_cast<std::underlying_type_t<T>>(rhs); \
+	}                                                                                                     \
+	constexpr std::underlying_type_t<T> operator~(T lhs)                                                  \
+	{                                                                                                     \
+		return ~static_cast<std::underlying_type_t<T>>(lhs);                                              \
+	}                                                                                                     \
+	inline std::underlying_type_t<T>& operator|=(T& lhs, T rhs)                                           \
+	{                                                                                                     \
+		auto& ulhs = reinterpret_cast<std::underlying_type_t<T>&>(lhs);                                   \
+		ulhs |= static_cast<std::underlying_type_t<T>>(rhs);                                              \
+		return ulhs;                                                                                      \
+	}                                                                                                     \
+	inline std::underlying_type_t<T>& operator&=(T& lhs, T rhs)                                           \
+	{                                                                                                     \
+		auto& ulhs = reinterpret_cast<std::underlying_type_t<T>&>(lhs);                                   \
+		ulhs &= static_cast<std::underlying_type_t<T>>(rhs);                                              \
+		return ulhs;                                                                                      \
+	}                                                                                                     \
+	inline std::underlying_type_t<T>& operator^=(T& lhs, T rhs)                                           \
+	{                                                                                                     \
+		auto& ulhs = reinterpret_cast<std::underlying_type_t<T>&>(lhs);                                   \
+		ulhs ^= static_cast<std::underlying_type_t<T>>(rhs);                                              \
+		return ulhs;                                                                                      \
+	}

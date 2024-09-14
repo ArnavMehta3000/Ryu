a56f0e7395b8af79b87ce2b5ce48b2e7c6f3fe7c
Arnav Mehta <arnavdatt@gmail.com>
2024-09-14 22:35:33 +0100

    Add new macro about target info

--- Tools/LoadMacros.bat
+++ Tools/LoadMacros.bat
@@ -6,5 +6,6 @@ xmake macro --import=Xmake/Macros/BuildAllConfigs.lua BuildAllConfigs
 xmake macro --import=Xmake/Macros/Clean.lua Clean
 xmake macro --import=Xmake/Macros/DeepClean.lua DeepClean
 xmake macro --import=Xmake/Macros/GenerateSolutionVS2022.lua GenVS
+xmake macro --import=Xmake/Macros/TargetsInfo.lua TargetInfo
 
 echo Finished loading macros
---
+++ Xmake/Macros/TargetsInfo.lua
@@ -0,0 +1,18 @@
+function PrintHeaders(header)
+    print("\n\n----- " .. header .. " -----")
+end
+
+function main(argv)
+	PrintHeaders("Core")
+	os.exec("xmake show -t RyuCore")
+	PrintHeaders("Engine")
+	os.exec("xmake show -t RyuEngine")
+	PrintHeaders("Input")
+	os.exec("xmake show -t RyuInput")
+	PrintHeaders("Graphics")
+	os.exec("xmake show -t RyuGraphics")
+	PrintHeaders("Editor")
+	os.exec("xmake show -t RyuEditor")
+	PrintHeaders("Testbench")
+	os.exec("xmake show -t RyuTestbench")
+end

7ac678d56801f755d356fe3ade80123c69c00e34
Arnav Mehta <arnavdatt@gmail.com>
2024-09-17 06:13:15 +0100

    Removed test plugin

--- Ryu/Plugins/Engine/RyuTest/xmake.lua
+++
@@ -1,8 +0,0 @@
-target("RyuTest")
-	add_rules("IncludeConfigs", "CommonPackages")
-	add_rules("RyuPlugin", { built_for = "Engine" })
-	add_includedirs("..", { public = true })
-	add_files("**.cpp")
-	add_headerfiles("**.h")
-	set_policy("build.fence", true)
-target_end()

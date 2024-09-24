4ed5b6451e13396042fdec5d08343c18d42aa7f6
Arnav Mehta <arnavdatt@gmail.com>
2024-09-24 02:27:00 +0100

    Updates

--- Ryu/Graphics/DX12/DX12CommandList.h
+++ Ryu/Graphics/DX12/DX12CommandList.h
@@ -14,6 +14,8 @@ namespace Ryu::Graphics
 		void Create(const DX12Device& device, D3D12_COMMAND_LIST_TYPE type = D3D12_COMMAND_LIST_TYPE_DIRECT);
 		void Release();
 
+		inline ID3D12GraphicsCommandList* GetGFXCmdList() const noexcept { return (ID3D12GraphicsCommandList*)Get(); }
+
 	private:
 		ComPtr<ID3D12CommandAllocator> m_allocator;
 	};

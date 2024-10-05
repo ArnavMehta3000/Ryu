17a866181c6e7eb250540061b09f04eb218b97e1
Arnav Mehta <arnavdatt@gmail.com>
2024-10-05 15:48:28 +0100

    Clean up xmake file

--- Ryu/Core/xmake.lua
+++ Ryu/Core/xmake.lua
@@ -10,20 +10,4 @@ target("RyuCore")
 	add_headerfiles("**.h")
 
 	add_links("Gdi32")
-
-	on_run(function (target)
-		-- Run the config parser for each toml file in the config directory
-		local root_dir = os.projectdir()
-		local input_dir = path.join(root_dir, "Config", "**.toml")
-		
-		for _, filedir in ipairs(os.filedirs(input_dir)) do
-			local output_filename = path.basename(filedir) .. ".json"
-			local output_dir = path.join(root_dir, "build", "Artifacts")
-			local output_file = path.join(output_dir, output_filename)
-			
-			cprint("Generating config file: %s", output_file)
-			os.execv("xmake run -q RyuConfigParser", { filedir, output_file })
-		end
-
-	end)
 target_end()

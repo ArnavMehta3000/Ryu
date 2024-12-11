8bdce3e363cd1c8b08059575c922c65d1a615309
Arnav Mehta <arnavdatt@gmail.com>
2024-12-11 00:16:51 +0000

    Add new Result type

--- Ryu/Common/Common/Common.h
+++ Ryu/Common/Common/Common.h
@@ -6,6 +6,7 @@
 #include "Common/Interfaces.h"
 #include "Common/Enum.h"
 #include "Common/API.h"
+#include "Common/Result.h"
 
 // Bring Ryu's standard types into the global namespace
 using namespace Ryu::Common::StandardTypes;

fa96ad816548c50441725fd87980c5333ad79f9b
Arnav Mehta <arnavdatt@gmail.com>
2024-12-11 01:29:53 +0000

    Added `assert-on-fail-hresult` option

--- Ryu/Graphics/xmake.lua
+++ Ryu/Graphics/xmake.lua
@@ -1,6 +1,10 @@
 target("RyuGraphics")
 	set_kind("static")
-	set_group("Ryu/Graphics")
+	set_group("Ryu/BreakOnFailHRESULT")
+
+	if has_config("assert-on-fail-hresult") then
+		add_defines("RYU_BREAK_ON_FAIL_HRESULT")
+	end
 
 	add_includedirs(".", { public = true })
 	add_files("**.cpp")
@@ -17,3 +21,9 @@ target("RyuGraphics")
 	)
 	add_links("d3d11", "d3d12", "dxgi", "dxguid")
 target_end()
+
+option("assert-on-fail-hresult")
+	set_showmenu(true)
+	set_description("Assert on failure of HRESULT")
+	set_category("root Ryu/Graphics")
+option_end()

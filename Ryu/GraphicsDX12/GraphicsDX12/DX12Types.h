a633081717503e3fc4a7780cdb0c205173e89216
Arnav Mehta <arnavdatt@gmail.com>
2025-01-03 23:10:57 +0400

    Update types namespace

--- Ryu/GraphicsDX12/GraphicsDX12/DX12Types.h
+++ Ryu/GraphicsDX12/GraphicsDX12/DX12Types.h
@@ -2,17 +2,14 @@
 #include "GraphicsRHI/API.h"
 #include <directx/d3dx12.h>
 
-namespace Ryu::Graphics
+namespace Ryu::Graphics::DX12
 {
-	namespace DX12
-	{
-		using IDX12Device              = ID3D12Device8;
-		using IDX12Resource            = ID3D12Resource;
-		using IDX12CommandQueue        = ID3D12CommandQueue;
-		using IDX12CommandAllocator    = ID3D12CommandAllocator;
-		using IDX12CommandList         = ID3D12CommandList;
-		using IDX12GraphicsCommandList = ID3D12GraphicsCommandList6;
-		using IDX12DescriptorHeap      = ID3D12DescriptorHeap;
-		using IDX12Fence               = ID3D12Fence;
-	}
+	using IDX12Device              = ID3D12Device8;
+	using IDX12Resource            = ID3D12Resource;
+	using IDX12CommandQueue        = ID3D12CommandQueue;
+	using IDX12CommandAllocator    = ID3D12CommandAllocator;
+	using IDX12CommandList         = ID3D12CommandList;
+	using IDX12GraphicsCommandList = ID3D12GraphicsCommandList6;
+	using IDX12DescriptorHeap      = ID3D12DescriptorHeap;
+	using IDX12Fence               = ID3D12Fence;
 }

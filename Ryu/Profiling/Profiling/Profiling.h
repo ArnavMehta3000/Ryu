fac5de9626272b2b46aed76a1586cd0d6f1aee05
Arnav Mehta <arnavdatt@gmail.com>
2025-03-31 21:57:05 +0100

    Final tracy integration

--- Ryu/Profiling/Profiling/Profiling.h
+++ Ryu/Profiling/Profiling/Profiling.h
@@ -1,8 +1,19 @@
 #pragma once
 #include <tracy/Tracy.hpp>
 
+#if defined(RYU_PROFILING_ENABLED)
+
 #define RYU_PROFILE_SCOPE() ZoneScoped
 #define RYU_PROFILE_SCOPEN(name) ZoneScopedN(name)
 #define RYU_PROFILE_SCOPENC(name, color) ZoneScopedNC(name, color)
 
-#define RYU_PROFILE_MARK_FRAME() FrameMark
+#define RYU_PROFILE_MARK_FRAME() FrameMark  
+
+#else
+#define RYU_PROFILE_SCOPE()
+#define RYU_PROFILE_SCOPEN(name)
+#define RYU_PROFILE_SCOPENC(name, color)
+
+#define RYU_PROFILE_MARK_FRAME()
+#endif 
+
--- Xmake/Packages.lua
+++ Xmake/Packages.lua
@@ -14,4 +14,4 @@ add_requires("Elos 98d44a142953be2eaab83030d3d1f527ebf81978")
 
 add_requires("toml++")
 
-add_requires("tracy")
+add_requires("tracy 5d542dc09f3d9378d005092a4ad446bd405f819a")

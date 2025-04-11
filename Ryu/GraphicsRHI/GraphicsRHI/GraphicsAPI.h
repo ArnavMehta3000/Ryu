1bf1e4053e5c30c28dda3e13e19a989e3a93ddd5
Arnav Mehta <arnavdatt@gmail.com>
2025-04-11 01:32:53 +0100

    Add undefined API type

--- Ryu/GraphicsRHI/GraphicsRHI/GraphicsAPI.h
+++ Ryu/GraphicsRHI/GraphicsRHI/GraphicsAPI.h
@@ -5,6 +5,7 @@ namespace Ryu::Graphics
 {
 	enum class API
 	{
+		Undefined,
 		DirectX11,
 		DirectX12
 	};
@@ -15,6 +16,7 @@ RYU_ENUM_TO_STRING(Ryu::Graphics::API,
 	switch (value)
 	{
 		using enum Ryu::Graphics::API;
+		case Undefined: return "Undefined";
 		case DirectX11: return "DirectX11";
 		case DirectX12: return "DirectX12";
 		default: return "<Unknown API>";

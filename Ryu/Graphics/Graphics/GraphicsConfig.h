9b8ae3630b8d57529d860418c6a1c223f523507e
Arnav Mehta <arnavdatt@gmail.com>
2025-04-24 02:52:07 +0100

    Add new graphics config

---
+++ Ryu/Graphics/Graphics/GraphicsConfig.h
@@ -0,0 +1,21 @@
+#include "Common/Globals.h"
+#include "Common/StandardTypes.h"
+#include "Config/Config.h"
+
+namespace Ryu::Gfx
+{
+	class GraphicsConfig : Config::ConfigBase
+	{
+		RYU_CONFIG_DECLARE(GraphicsConfig, "GraphicsConfig.toml");
+		RYU_CONFIG_DECLARE_SECTION(Rendering);
+		RYU_CONFIG_DECLARE_SECTION(Debugging);
+
+	public:
+		Config::ConfigValue<bool> EnableDebugLayer{ this, DebuggingSection, "EnableDebugLayer", Common::Globals::g_isDebug };
+		Config::ConfigValue<bool> EnableGPUBasedValidation{ this, DebuggingSection, "EnableGPUBasedValidation", false };
+		Config::ConfigValue<bool> EnableVSync{ this, RenderingSection, "EnableVSync", true };
+		Config::ConfigValue<bool> AllowTearing{ this, RenderingSection, "AllowTearing", false };
+
+		static constexpr u32 FRAME_COUNT = 2;
+	};
+}

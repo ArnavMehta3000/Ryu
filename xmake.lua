20c03119ba9762312b58d0329ead10a2ce8f218d
Arnav Mehta <arnavdatt@gmail.com>
2025-05-02 00:19:11 +0100

    Change runtime libraries

--- xmake.lua
+++ xmake.lua
@@ -39,7 +39,10 @@ set_policy("package.requires_lock", false)
 set_policy("build.always_update_configfiles", false)
 
 if is_mode("debug", "releasedbg") then
-	add_defines("RYU_BUILD_DEBUG")
+	add_defines("RYU_BUILD_DEBUG", "_DEBUG")
+	set_runtimes("MDd")
+else
+	set_runtimes("MD")
 end
 
 -- Add common window definitions

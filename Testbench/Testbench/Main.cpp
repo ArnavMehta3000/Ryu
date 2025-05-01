07df1165971623cfed5f18b2f21497203a3e4394
Arnav Mehta <arnavdatt@gmail.com>
2025-05-02 00:19:46 +0100

    Enable crt memory tracking for testbench

--- Testbench/Testbench/Main.cpp
+++ Testbench/Testbench/Main.cpp
@@ -1,10 +1,19 @@
 #include "Engine/EntryPoint.h"
 #include "Test.h"
 
+#if defined(RYU_BUILD_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
 RYU_USE_APP(TestApp)
 
 RYU_MAIN()
 {
+#if defined(RYU_BUILD_DEBUG)  // Enable memory leak checking
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
 	Ryu::Engine::Runner
 	{
 		.ProjectDir = RYU_ROOT_DIR "/Testbench"

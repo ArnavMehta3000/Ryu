f9c34b91ead4e64842231984a71e305e131dec3d
Arnav Mehta <arnavdatt@gmail.com>
2025-06-08 04:33:10 +0100

    Fix graphics config formatting

--- Ryu/Graphics/Graphics/GraphicsConfig.h
+++ Ryu/Graphics/Graphics/GraphicsConfig.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Common/Globals.h"
-#include "Common/StandardTypes.h"
 #include "Config/Config.h"
 
 namespace Ryu::Gfx
@@ -12,10 +11,10 @@ namespace Ryu::Gfx
 		RYU_CONFIG_DECLARE_SECTION(Debugging);
 
 	public:
-		Config::ConfigValue<bool> EnableDebugLayer{ this, DebuggingSection, "EnableDebugLayer", Common::Globals::g_isDebug };
-		Config::ConfigValue<bool> EnableGPUBasedValidation{ this, DebuggingSection, "EnableGPUBasedValidation", false };
-		Config::ConfigValue<bool> EnableVSync{ this, RenderingSection, "EnableVSync", true };
-		Config::ConfigValue<bool> AllowTearing{ this, RenderingSection, "AllowTearing", false };
-		Config::ConfigValue<bool> UseWARP{ this, RenderingSection, "UseWARP", false };
+		Config::ConfigValue<bool> EnableDebugLayer        { this, DebuggingSection, "EnableDebugLayer", Common::Globals::g_isDebug };
+		Config::ConfigValue<bool> EnableGPUBasedValidation{ this, DebuggingSection, "EnableGPUBasedValidation", false              };
+		Config::ConfigValue<bool> EnableVSync             { this, RenderingSection, "EnableVSync", true                            };
+		Config::ConfigValue<bool> AllowTearing            { this, RenderingSection, "AllowTearing", false                          };
+		Config::ConfigValue<bool> UseWARP                 { this, RenderingSection, "UseWARP", false                               };
 	};
 }

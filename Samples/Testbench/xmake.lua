eb3ea547a823a646f03d4ee9396eb24194421758
Arnav Mehta <arnavdatt@gmail.com>
2025-07-14 04:07:35 +0100

    Remove GameInput nuget package

--- Samples/Testbench/xmake.lua
+++ Samples/Testbench/xmake.lua
@@ -3,14 +3,15 @@ target("RyuTestbench")
 
 	set_default(not get_config("ryu-build-with-editor"))
 	set_group("Ryu/Testing")
-	add_packages("GameInput")
+	add_packages()
 	add_includedirs(".")
 	add_files("Testbench/**.cpp", { unity_group = "Testbench" })
 	add_extrafiles("Scripts/**.as")
 	add_headerfiles("Testbench/**.h")
 
 	add_deps(
-		"RyuEngine"
+		"RyuEngine",
+		"GameInput"  -- External deps
 		--,"RyuTestbenchEnginePlugin"
 	)
 target_end()

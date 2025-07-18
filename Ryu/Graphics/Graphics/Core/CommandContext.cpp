03a7e9b6d268af0dc8f9b96fc8be92e83e27b9a0
Arnav Mehta <arnavdatt@gmail.com>
2025-07-18 02:15:34 +0100

    Fix command context

--- Ryu/Graphics/Graphics/Core/CommandContext.cpp
+++ Ryu/Graphics/Graphics/Core/CommandContext.cpp
@@ -4,28 +4,4 @@
 
 namespace Ryu::Gfx
 {
-	CommandContext::CommandContext(std::weak_ptr<Device> parent, CommandListType type)
-		: DeviceObject(parent)
-		, m_type(type)
-		, m_frameIndex(0)
-	{
-		OnConstruct(type);
-	}
-
-	CommandContext::~CommandContext()
-	{
-		OnDestruct();
-	}
-
-	void CommandContext::OnConstruct(CommandListType type)
-	{
-		RYU_PROFILE_SCOPE();
-
-		if (auto parent = GetParent())
-		{
-			const D3D12_COMMAND_LIST_TYPE nativeType = DX12::ToNative(type);
-			DX12::Device* const device = parent->GetDevice();
-			DXCallEx(device->CreateCommandAllocator(nativeType, IID_PPV_ARGS(&m_commandAllocator)), parent->GetDevice());
-		}
-	}
 }
--- Ryu/Graphics/Graphics/Core/CommandContext.h
+++ Ryu/Graphics/Graphics/Core/CommandContext.h
@@ -5,20 +5,5 @@ namespace Ryu::Gfx
 {
 	class CommandContext
 	{
-		RYU_GFX_DEVICE_OBJ;
-	public:
-		CommandContext() = default;
-		CommandContext(std::weak_ptr<Device> parent);
-		~CommandContext();
-
-		void Begin();
-		void End();
-
-	private:
-		ComPtr<DX12::GraphicsCommandList> m_commandList;
-		ComPtr<DX12::CommandAllocator> m_commandAllocator;
-		CommandListType m_type;
-		u32 m_frameIndex;
-		std::vector<CD3DX12_RESOURCE_BARRIER> m_pendingBarriers;
 	};
 }

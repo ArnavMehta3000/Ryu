44ff2e1a3fc6bfdc653bbd41c648b827795c5df7
Arnav Mehta <arnavdatt@gmail.com>
2025-07-18 02:12:42 +0100

    Clean command context

--- Ryu/Graphics/Graphics/Core/CommandContext.h
+++ Ryu/Graphics/Graphics/Core/CommandContext.h
@@ -3,35 +3,16 @@
 
 namespace Ryu::Gfx
 {
-	class CommandContext : public DeviceObject<CommandContext>
+	class CommandContext
 	{
 		RYU_GFX_DEVICE_OBJ;
 	public:
 		CommandContext() = default;
-		CommandContext(std::weak_ptr<Device> parent, CommandListType type = CommandListType::Direct);
+		CommandContext(std::weak_ptr<Device> parent);
 		~CommandContext();
 
 		void Begin();
 		void End();
-		void Execute();
-		void Reset();
-
-		void TransitionResource(DX12::Resource* resource, D3D12_RESOURCE_STATES before, D3D12_RESOURCE_STATES after);
-		void FlushResourceBarriers();
-
-		void SetRenderTarget(const D3D12_CPU_DESCRIPTOR_HANDLE& rtv, const D3D12_CPU_DESCRIPTOR_HANDLE* dsv = nullptr);
-		void ClearRenderTarget(const D3D12_CPU_DESCRIPTOR_HANDLE& rtv, const float* clearColor);
-		void ClearDepthStencil(const D3D12_CPU_DESCRIPTOR_HANDLE& dsv, float depth = 1.0f, u8 stencil = 0);
-
-		void SetViewport(const D3D12_VIEWPORT& viewport);
-		void SetScissorRect(const D3D12_RECT& rect);
-
-		inline DX12::GraphicsCommandList* GetCommandList() const { return m_commandList.Get(); }
-		inline u32 GetFrameIndex() const { return m_frameIndex; }
-
-	private:
-		void OnConstruct(CommandListType type = CommandListType::Direct);
-		void OnDestruct();
 
 	private:
 		ComPtr<DX12::GraphicsCommandList> m_commandList;

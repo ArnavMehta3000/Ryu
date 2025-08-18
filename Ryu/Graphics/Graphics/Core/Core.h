42b6dc6396f2da81e4e3b0d444c62bd8ff4ac108
Arnav Mehta <arnavdatt@gmail.com>
2025-08-18 14:36:58 +0100

    Add dummy resize function

--- Ryu/Graphics/Graphics/Core/Core.h
+++ Ryu/Graphics/Graphics/Core/Core.h
@@ -6,6 +6,8 @@ namespace Ryu::Gfx::Core
 	void Init(HWND window, DXGI_FORMAT backBufferFormat);
 	void Shutdown();
 
+	inline void Resize(u32, u32) { /*TODO*/ }
+
 	const CD3DX12FeatureSupport& GetFeatureSupport();
 	const ComPtr<DX12::Device>& GetDevice();
 	const ComPtr<DXGI::Factory>& GetFactory();

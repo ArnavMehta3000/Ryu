6339e3b554c72689002c5a37010fc3b7a4a227af
Arnav Mehta <arnavdatt@gmail.com>
2025-08-20 18:36:29 +0100

    Update xmake options for setting log level

--- Ryu/Logging/xmake.lua
+++ Ryu/Logging/xmake.lua
@@ -7,25 +7,7 @@ target("RyuLogging")
 
 	add_packages("spdlog", { public = true })
 	add_deps("RyuUtils", "RyuGlobals")
-	add_options(
-		"ryu-log-trace-enabled",
-		"ryu-log-debug-enabled",
-		"ryu-log-info-enabled",
-		"ryu-log-warn-enabled")
-
-	-- Compile time switch to toggle logging
-	if get_config("ryu-log-trace-enabled") then
-		add_defines("RYU_LOG_ENABLED_TRACE", { public = true })
-	end
-	if get_config("ryu-log-debug-enabled") then
-		add_defines("RYU_LOG_ENABLED_DEBUG", { public = true })
-	end
-	if get_config("ryu-log-info-enabled") then
-		add_defines("RYU_LOG_ENABLED_INFO", { public = true })
-	end
-	if get_config("ryu-log-warn-enabled") then
-		add_defines("RYU_LOG_ENABLED_WARN", { public = true })
-	end
+	add_options("ryu-log-level")
 
 	if has_config("ryu-unity-build") then
 		add_rules("c++.unity_build")

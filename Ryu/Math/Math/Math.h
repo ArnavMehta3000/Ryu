7eb1c64d61ab0d59ce6a3d3eab39238770f0899b
Arnav Mehta <a.mehta@flixinteractive.com>
2025-10-23 16:38:33 +0100

    Expose simple math in Math namespace

--- Ryu/Math/Math/Math.h
+++ Ryu/Math/Math/Math.h
@@ -6,6 +6,8 @@ namespace SM = DirectX::SimpleMath;
 
 namespace Ryu::Math
 {
+	using namespace DirectX::SimpleMath;
+
 	constexpr auto BytesToKB(auto bytes) { return bytes / 1024.0f; }
 	constexpr auto BytesToMB(auto bytes) { return BytesToKB(bytes) / 1024.0f; }
 	constexpr auto BytesToGB(auto bytes) { return BytesToMB(bytes) / 1024.0f; }

df8a56b9f01dd8c171249f8ce60754ce7adeb32c
Arnav Mehta <arnavdatt@gmail.com>
2026-01-07 01:04:23 +0400

    Remove old renderer code
    Plus some cleanup

--- Ryu/Graphics/RenderFrameBuilder.cpp
+++ Ryu/Graphics/RenderFrameBuilder.cpp
@@ -1,190 +1,214 @@
 #include "Graphics/RenderFrameBuilder.h"
-#include "Graphics/Camera.h"
-#include "Game/World/World.h"
+
+#include "Core/Profiling/Profiling.h"
 #include "Game/Components/CameraComponent.h"
-#include "Game/Components/TransformComponent.h"
 #include "Game/Components/MeshRenderer.h"
+#include "Game/Components/TransformComponent.h"
+#include "Game/World/World.h"
+#include "Graphics/Camera.h"
 #include <ranges>
 
 namespace Ryu::Gfx
 {
-	RenderFrameBuilder::RenderFrameBuilder(Asset::AssetRegistry* registry)
-		: m_assetRegistry(registry)
+	RenderFrameBuilder& RenderFrameBuilder::SetAssetRegistry(Asset::AssetRegistry* registry)
 	{
+		m_assetRegistry = registry;
+		return *this;
 	}
 
-	RenderFrame RenderFrameBuilder::ExtractRenderData(Game::World& world, f32 deltaTime, f32 TotalTime, u64 frameNumber)
+	RenderFrameBuilder& RenderFrameBuilder::SetScreenSize(u32 width, u32 height)
 	{
-		RenderFrame frame{};
-		frame.DeltaTime  = deltaTime;
-		frame.TotalTime  = TotalTime;
-		frame.FrameNumber= frameNumber;
+		m_config.ScreenWidth = width;
+		m_config.ScreenHeight = height;
+		return *this;
+	}
+
+	f32 RenderFrameBuilder::GetAspectRatio() const
+	{
+		return m_config.ScreenHeight > 0
+			? static_cast<f32>(m_config.ScreenWidth) / static_cast<f32>(m_config.ScreenHeight)
+			: 1.0f;
+	}
+
+	RenderFrame RenderFrameBuilder::ExtractRenderData(Game::World& world, const Utils::FrameTimer& timer)
+	{
+		RYU_PROFILE_SCOPE();
+
+		RenderFrame frame
+		{
+			.DeltaTime   = timer.DeltaTimeF(),
+			.TotalTime   = (f32)timer.TimeSinceStart<std::chrono::seconds>(),
+			.FrameNumber = timer.FrameCount()
+		};
 
 		// Collect all cameras, sorted by priority
 		std::vector<CameraData> cameras;
 		CollectCameras(world, cameras);
 
-		// If no camera components exist, we use a default implementation
-		// That's handled at a higher level (WorldRenderer)
+		// If no camera components exist, SceneRenderer will use its default camera
 
-        // For each camera, collect visible renderables
-        for (const auto& camera : cameras)
-        {
-            RenderView view{};
-            view.CameraData = camera;
+		// For each camera, collect visible renderables
+		for (const auto& camera : cameras)
+		{
+			RenderView view{};
+			view.CameraData = camera;
 
-            CollectRenderables(
-                world,
-                camera.CullingMask,
-                view.OpaqueItems,
-                view.TransparentItems);
+			CollectRenderables(
+				world,
+				camera.CullingMask,
+				view.OpaqueItems,
+				view.TransparentItems);
 
-            // Sort opaque front-to-back (minimize overdraw)
-            std::ranges::sort(view.OpaqueItems,
-            [](const RenderItem& a, const RenderItem& b)
-            {
-                return a.SortKey < b.SortKey;
-            });
+			// Sort opaque front-to-back (minimize overdraw)
+			std::ranges::sort(view.OpaqueItems,
+			[](const RenderItem& a, const RenderItem& b)
+			{
+				return a.SortKey < b.SortKey;
+			});
 
-            // Sort transparent back-to-front (correct blending)
-            std::ranges::sort(view.TransparentItems,
-            [](const RenderItem& a, const RenderItem& b)
-            {
-                return a.SortKey > b.SortKey;
-            });
-            
+			// Sort transparent back-to-front (correct blending)
+			std::ranges::sort(view.TransparentItems,
+			[](const RenderItem& a, const RenderItem& b)
+			{
+				return a.SortKey > b.SortKey;
+			});
 
-            frame.Views.push_back(std::move(view));
-        }
+			frame.Views.push_back(std::move(view));
+		}
 
-        return frame;
+		return frame;
 	}
 
-    RenderView RenderFrameBuilder::ExtractViewForCamera(Game::World& world, const CameraData& camera)
-    {
-        RenderView view;
-        view.CameraData = camera;
+	RenderView RenderFrameBuilder::ExtractViewForCamera(Game::World& world, const CameraData& camera)
+	{
+		RYU_PROFILE_SCOPE();
 
-        CollectRenderables(
-            world,
-            camera.CullingMask,
-            view.OpaqueItems,
-            view.TransparentItems);
+		RenderView view { .CameraData = camera };
 
-        return view;
-    }
+		CollectRenderables(
+			world,
+			camera.CullingMask,
+			view.OpaqueItems,
+			view.TransparentItems);
 
-    void RenderFrameBuilder::CollectRenderables(Game::World& world, u32 cullingMask, std::vector<RenderItem>& opaqueOut, std::vector<RenderItem>& transparentOut)
-    {
-        UNREFERENCED_PARAMETER(transparentOut);
+		return view;
+	}
 
-        auto& registry = world.GetRegistry();
+	void RenderFrameBuilder::CollectRenderables(Game::World& world, u32 cullingMask, std::vector<RenderItem>& opaqueOut, 
+		[[maybe_unused]] std::vector<RenderItem>& transparentOut)  // Temporary until I figure out transparency
+	{
+		RYU_PROFILE_SCOPE();
 
-        // Query all entities with Transform and MeshRenderer
-        auto view = registry.view<Game::Transform, Game::MeshRenderer>();
+		auto& registry = world.GetRegistry();
 
-        for (const auto& [entity, transform, renderer] :  view.each())
-        {
-            if (!renderer.IsVisible                                      // Skip invisible
-                || ((cullingMask & (1u << renderer.RenderLayer)) == 0))  // Layer culling
-            {
-                continue;
-            }
+		// Query all entities with Transform and MeshRenderer
+		auto view = registry.view<Game::Transform, Game::MeshRenderer>();
 
-            RenderItem item = CreateRenderItem(transform, renderer);
-            item.SortKey = ComputeSortKey(item);
+		for (const auto& [entity, transform, renderer] : view.each())
+		{
+			if (!renderer.IsVisible                                      // Skip invisible
+				|| ((cullingMask & (1u << renderer.RenderLayer)) == 0))  // Layer culling
+			{
+				continue;
+			}
 
-            RYU_TODO("Check material for transparency")
-            // For now, all items are opaque
-            opaqueOut.push_back(item);
-        }
-    }
+			RenderItem item = CreateRenderItem(transform, renderer);
+			item.SortKey = ComputeSortKey(item);
 
-    void RenderFrameBuilder::CollectCameras(Game::World& world, std::vector<CameraData>& camerasOut)
-    {
-        auto& registry = world.GetRegistry();
-        auto view = registry.view<Game::Transform, Game::Camera>();
+			// For now, all items are opaque
+			opaqueOut.push_back(item);
+		}
+	}
 
-        RYU_TODO("Remove these hard-coded numbers for testing and get actual screen size from somewhere")
-        constexpr u32 screenWidth = 1280;
-        constexpr u32 screenHeight = 720;
+	void RenderFrameBuilder::CollectCameras(Game::World& world, std::vector<CameraData>& camerasOut)
+	{
+		RYU_PROFILE_SCOPE();
 
-        for (const auto& [entity, transform, camera] : view.each())
-        {
-            const CameraData camData = ExtractCameraData(transform, camera, screenWidth, screenHeight);
-            camerasOut.push_back(camData);
-        }
+		auto& registry = world.GetRegistry();
+		auto view = registry.view<Game::Transform, Game::Camera>();
 
-        // Sort by priority
-        std::sort(camerasOut.begin(), camerasOut.end(),
-        [](const CameraData& a, const CameraData& b)
-        {
-            return a.Priority < b.Priority;
-        });
-    }
+		for (const auto& [entity, transform, camera] : view.each())
+		{
+			const CameraData camData = ExtractCameraData(transform, camera);
+			camerasOut.push_back(camData);
+		}
 
-    CameraData RenderFrameBuilder::ExtractCameraData(const Game::Transform& transform, const Game::Camera& camera, u32 screenWidth, u32 screenHeight)
-    {
-        CameraData data{};
+		// Sort by priority
+		std::ranges::sort(camerasOut, [](const CameraData& a, const CameraData& b) { return a.Priority < b.Priority; });
+	}
 
-        // Position/orientation from entity Transform
-        data.Position     = transform.Position;
-        SM::Matrix rotMat = SM::Matrix::CreateFromQuaternion(transform.Rotation);
-        data.Forward      = SM::Vector3(rotMat._31, rotMat._32, rotMat._33);
-        SM::Vector3 up    = SM::Vector3(rotMat._21, rotMat._22, rotMat._23);
+	CameraData RenderFrameBuilder::ExtractCameraData(const Game::Transform& transform, const Game::Camera& camera)
+	{
+		RYU_PROFILE_SCOPE();
+		CameraData data{};
 
-        // View matrix from transform
-        data.ViewMatrix = SM::Matrix::CreateLookAt(
-            transform.Position,
-            transform.Position + data.Forward,
-            up);
+		// Position/orientation from entity Transform
+		data.Position     = transform.Position;
+		SM::Matrix rotMat = SM::Matrix::CreateFromQuaternion(transform.Rotation);
+		data.Forward      = SM::Vector3(rotMat._31, rotMat._32, rotMat._33);
+		SM::Vector3 up    = SM::Vector3(rotMat._21, rotMat._22, rotMat._23);
 
-        RYU_TODO("Better rework this logic to make use of the camera provided in Gfx or split the logic into a camera controller")
-        // Projection from CameraComponent settings
-        f32 aspect = (f32)screenWidth/ (f32)screenHeight;
-        if (camera.Mode == Game::Camera::ProjectionMode::Perspective)
-        {
-            data.ProjectionMatrix = SM::Matrix::CreatePerspectiveFieldOfView(
-                DirectX::XMConvertToRadians(camera.FieldOfView),
-                aspect, camera.NearPlane, camera.FarPlane);
-        }
-        else
-        {
-            data.ProjectionMatrix = SM::Matrix::CreateOrthographic(
-                camera.OrthoSize * aspect * 2.0f,
-                camera.OrthoSize * 2.0f,
-                camera.NearPlane, camera.FarPlane);
-        }
+		// View matrix from transform
+		data.ViewMatrix = SM::Matrix::CreateLookAt(
+			transform.Position,
+			transform.Position + data.Forward,
+			up);
 
-        data.ViewProjectionMatrix = data.ViewMatrix * data.ProjectionMatrix;
-        data.ViewportWidth        = screenWidth;
-		data.ViewportHeight       = screenHeight;
-        return data;
-    }
+		// Projection from CameraComponent settings using stored screen size
+		const f32 aspect = GetAspectRatio();
 
-    RenderItem RenderFrameBuilder::CreateRenderItem(const Game::Transform& transform, const Game::MeshRenderer& renderer)
-    {
-        return RenderItem
-        {
-            .MeshHandle     = renderer.MeshHandle,
-            .WorldTransform = transform.GetWorldMatrix(),
-            .RenderLayer    = renderer.RenderLayer
-        };
-    }
+		if (camera.Mode == Game::Camera::ProjectionMode::Perspective)
+		{
+			data.ProjectionMatrix = SM::Matrix::CreatePerspectiveFieldOfView(
+				DirectX::XMConvertToRadians(camera.FieldOfView),
+				aspect,
+				camera.NearPlane,
+				camera.FarPlane);
+		}
+		else
+		{
+			data.ProjectionMatrix = SM::Matrix::CreateOrthographic(
+				camera.OrthoSize * aspect * 2.0f,
+				camera.OrthoSize * 2.0f,
+				camera.NearPlane,
+				camera.FarPlane);
+		}
 
-    u64 RenderFrameBuilder::ComputeSortKey(const RenderItem& item)
-    {
-        // Sort key encoding (64 bits):
-        // [63-56] Render layer (8 bits)
-        // [55-32] Material handle (24 bits) - for batching
-        // [31-0]  Mesh handle (32 bits) - for batching
+		data.ViewProjectionMatrix = data.ViewMatrix * data.ProjectionMatrix;
+		data.NearPlane            = camera.NearPlane;
+		data.FarPlane             = camera.FarPlane;
+		data.CullingMask          = camera.CullingMask;
+		data.Priority             = camera.Priority;
 
-        u64 key = 0;
-        key |= static_cast<u64>(item.RenderLayer) << 56;
-        // Future material and mesh logic
-        //key |= static_cast<u64>(item.Material.Index & 0xFFFFFF) << 32;
-        //key |= static_cast<u64>(item.Mesh.Index);
-        //
-        return key;
-    }
+		// NOTE: Viewport is fullscreen (may be different for ImGui in the future?)
+		data.ViewportX      = 0;
+		data.ViewportY      = 0;
+		data.ViewportWidth  = m_config.ScreenWidth;
+		data.ViewportHeight = m_config.ScreenHeight;
+
+		return data;
+	}
+
+	RenderItem RenderFrameBuilder::CreateRenderItem(const Game::Transform& transform, const Game::MeshRenderer& renderer)
+	{
+		return RenderItem
+		{
+			.MeshHandle     = renderer.MeshHandle,
+			.WorldTransform = transform.GetWorldMatrix(),
+			.RenderLayer    = renderer.RenderLayer
+		};
+	}
+
+	u64 RenderFrameBuilder::ComputeSortKey(const RenderItem& item)
+	{
+		// Sort key encoding (64 bits):
+		// [63-56] Render layer (8 bits)
+		// [55-32] Material handle (24 bits) - for batching
+		// [31-0]  Mesh handle (32 bits) - for batching
+
+		u64 key = 0;
+		key |= static_cast<u64>(item.RenderLayer) << 56;
+		// Future: add material and mesh indices for batching
+		return key;
+	}
 }

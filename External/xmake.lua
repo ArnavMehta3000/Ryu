6284a9179fb6af9572e981d2cb89efd2c9f8350f
Arnav Mehta <arnavdatt@gmail.com>
2026-01-20 19:18:24 +0400

    Change externals to static libraries

--- External/xmake.lua
+++ External/xmake.lua
@@ -1,7 +1,7 @@
 -------------------- Simple Math --------------------
 target("SimpleMath")
 	set_group("Ryu/External")
-	set_kind("object")
+	set_kind("static")
 	add_headerfiles("SimpleMath/SimpleMath.h", { public = true })
 	add_files("SimpleMath/SimpleMath.cpp", { public = true })
 	add_includedirs(".", { public = true })
@@ -19,7 +19,7 @@ target_end()
 -------------------- ImGui --------------------
 target("ImGui")
 	set_group("Ryu/External")
-	set_kind("object")
+	set_kind("static")
 	add_headerfiles(
 		"ImGui/*.h",
 		"ImGui/backends/*.h", { public = true })
@@ -34,7 +34,7 @@ target_end()
 -------------------- STB --------------------
 target("STB")
 	set_group("Ryu/External")
-	set_kind("object")
+	set_kind("static")
 	add_headerfiles("STB/*.h", { public = true })
 	add_files("STB/*.cpp", { public = true })
 	add_includedirs(".", { public = true })
@@ -42,7 +42,7 @@ target_end()
 
 target("TinyOBJ")
 	set_group("Ryu/External")
-	set_kind("object")
+	set_kind("static")
 	add_headerfiles("TinyOBJ/*.h", { public = true })
 	add_files("TinyOBJ/*.cpp", { public = true })
 	add_includedirs(".", { public = true })
